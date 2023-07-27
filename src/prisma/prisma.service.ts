f446c41b5461f5dba64c1b814bd47043d478c4e2
Romi Julianto <romyjulians@gmail.com>
2023-07-28 06:55:54 +0700

    feat(prisma-client): better handle service in prisma client

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    // approach connect prisma client as soon as the module initialize
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    // ensure prisma client is prperly closed when app exit
+    async enableShutdownHooks(app: INestApplication) {
+        const prisma = this as any;
+        prisma.$on('beforeExit', async () => {
+            await prisma.$disconnect();
+            await app.close();
+        });
+    }
+}
