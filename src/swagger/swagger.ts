f4747ef15dfcf40efc7dd89ede4430b75d1732aa
Romi Julianto <romyjulians@gmail.com>
2023-07-29 21:42:41 +0700

    docs: add swagger docs options

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -2,14 +2,18 @@ import { INestApplication } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication) {
-    // setting up swagger api/documentation
+    // TODO: setting up swagger api/documentation
     const config = new DocumentBuilder()
         .setTitle('Median')
         .setDescription('The Median API description')
-        .setVersion('0.1')
-        .setBasePath('api/doc')
+        .setVersion('1.0')
+        .addTag('main', 'base url')
+        .addTag('articles', 'endpoint for article transaction')
+        .setBasePath('api/docs')
+        .addServer('http://localhost:3000', 'HTTP')
+        .addServer('https://localhost:3000', 'HTTPS')
         .setContact('Romi Julianto', 'https://www.linkedin.com/in/romijulianto/', 'romyjulians@gmail.com')
-        .build()
+        .build();
 
     const document = SwaggerModule.createDocument(app, config);
 
@@ -18,5 +22,18 @@ export function setupSwagger(app: INestApplication) {
             displayRequestDuration: true,
             filter: true,
         },
+        customSiteTitle: 'Median Backend',
+        swaggerUrl: 'http://localhost:3000/api/docs/',
+        explorer: true,
+        url: 'http://localhost:3000/',
+        useGlobalPrefix: true,
+        customCss: '',
+        customCssUrl: '',
+        customJs: '',
+        customJsStr: '',
+        customfavIcon: '',
+        validatorUrl: '',
+        jsonDocumentUrl: '',
+        yamlDocumentUrl: '',
     })
 }
