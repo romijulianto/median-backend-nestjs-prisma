4f71dfd506c70d07618de8c77faa29fb16759db3
Romi Julianto <romyjulians@gmail.com>
2023-07-30 18:11:41 +0700

    feat: add PORT to env

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setupSwagger(app); // TODO:running and setup swagger
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
