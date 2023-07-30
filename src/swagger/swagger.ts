a31219035ed9237fd7a7336c3df22c1d31b666de
Romi Julianto <romyjulians@gmail.com>
2023-07-30 18:24:39 +0700

    feat: add railway server

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -11,7 +11,7 @@ export function setupSwagger(app: INestApplication) {
         .addTag('articles', 'endpoint for article transaction')
         .setBasePath('api/docs')
         .addServer('http://localhost:3000', 'HTTP')
-        .addServer('https://localhost:3000', 'HTTPS')
+        .addServer('https://median-backend.up.railway.app', 'HTTPS')
         .setContact('Romi Julianto', 'https://www.linkedin.com/in/romijulianto/', 'romyjulians@gmail.com')
         .build();
 
