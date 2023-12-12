3fe20d5ca92becc6b185c5d9d69fced8568e9921
Romi Julianto <romyjulians@gmail.com>
2023-12-12 09:58:20 +0700

    feat: prevent any properties doesn't define validation decorator

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   /*TODO: add validation pipe */
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('median');
   setupSwagger(app); // TODO:running and setup swagger
   await app.listen(process.env.PORT, '0.0.0.0');
