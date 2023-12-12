dc93f58fce8ea3eb7cbeb0368ff629bca5a12f6c
Romi Julianto <romyjulians@gmail.com>
2023-12-12 09:53:12 +0700

    feat: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /*TODO: add validation pipe */
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('median');
   setupSwagger(app); // TODO:running and setup swagger
   await app.listen(process.env.PORT, '0.0.0.0');
